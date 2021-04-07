e0c1118a855d16cad036ef89b06eb61de14b0dc6
Danny Guevara <ga19014@ues.edu.sv>
2021-04-07 17:24:04 -0600

    Tercer Ejercicio Solucionado

---
+++ Ejercicio-3/app.js
@@ -0,0 +1,32 @@
+const arr = [13,5,25,28,24,22,21,19];
+let size = arr.length;
+let x = 30;
+let k_esimo = (arr,x) =>{
+    console.log("ARRAY SIN EL CAMBIO DE VALOR  K-ESIMO");
+    console.log(arr);
+    let aux;
+    let Mayor = 0;
+
+    for(let i =0; i < size; i++){
+        if(arr[i] > Mayor){
+            Mayor = arr[i];
+        }
+        
+    }
+    console.log("El dato mayor "+Mayor)
+
+    for(let j =0; j < size; j++){
+        if(arr[j] == Mayor){
+            aux = arr[j];
+            arr[j] = x;
+            x = aux;
+        }
+       
+    }   
+   console.log("Valor del axu "+aux);
+
+   console.log("ARRAY CON EL CAMBIO DE VALOR EN LA K-ESIMA");
+   console.log(arr);
+}
+
+k_esimo(arr,x);
--- README.md
+++ README.md
@@ -8,3 +8,5 @@ arrow functions para realizar las operaciones donde sea requerido.
 <h2>1- Leer una matriz de 3 por 3 elementos y calcular la suma de cada una de sus filas y columnas,
 dejando dichos resultados en dos vectores, uno de la suma de las filas y otro de las columnas.</h2>
 <h2>2- Calcular la suma de los elementos de la diagonal principal de una matriz cuatro por cuatro (4 ×4).</h2>
+<h2>3- Se dispone de una lista (vector) de N elementos. Se desea diseñar un algoritmo que permita
+insertar el valor x en el lugar k-ésimo de la mencionada lista.</h2>
